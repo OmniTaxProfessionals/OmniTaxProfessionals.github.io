b259147c111519d78ba9de3b34f968c0feaac516
Sam Garg <132874866+SamGargRobotics@users.noreply.github.com>
2025-12-20 21:41:57 +1000

    Updated Version

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyBOND-wraRXGL0jB8xK2zHnDMBXBhA5WRk",
     authDomain: "otp-client-login.firebaseapp.com",
     projectId: "otp-client-login",
-    version: "v-alpha" // Set your version here
+    version: "v-1.01, Created by Sam Garg" // Set your version here
 };
 
 export default firebaseConfig;
