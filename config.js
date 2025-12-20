6c086fea00f32f7fe0756d9d76c184982ada303e
Sam Garg <132874866+SamGargRobotics@users.noreply.github.com>
2025-12-20 21:38:57 +1000

    Added Versions

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 const firebaseConfig = {
     apiKey: "AIzaSyBOND-wraRXGL0jB8xK2zHnDMBXBhA5WRk",
     authDomain: "otp-client-login.firebaseapp.com",
-    projectId: "otp-client-login"
-    // Add storageBucket and appId here if you use them!
+    projectId: "otp-client-login",
+    version: "v-alpha" // Set your version here
 };
 
 export default firebaseConfig;
