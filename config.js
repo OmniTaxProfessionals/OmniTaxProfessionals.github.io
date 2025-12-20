8e23b53d1a46fe1ed36068f3705b77f979b989cc
Sam Garg <132874866+SamGargRobotics@users.noreply.github.com>
2025-12-20 21:16:46 +1000

    Removed .gitignore

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyBOND-wraRXGL0jB8xK2zHnDMBXBhA5WRk",
+    authDomain: "otp-client-login.firebaseapp.com",
+    projectId: "otp-client-login"
+    // Add storageBucket and appId here if you use them!
+};
+
+export default firebaseConfig;
